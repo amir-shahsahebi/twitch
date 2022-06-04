3765e6ba2692b7826cc69f526d7d387457298fea
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-04 16:39:38 +0430

    redux form implemented in input

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,27 @@
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+import React from "react";
+// for installing redux-form use: npm install redux-form --legacy-peer-deps
+import { Field, reduxForm } from "redux-form";
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return (
+      <input {...formProps.input} /> // we can use this new syntax instead of line below
+      // <input
+      //   onChange={formProps.input.onChange}
+      //   value={formProps.input.value}
+      // />
+    );
+  }
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
-
+// for installing redux form, we use:   npm install redux-form --legacy-peer-deps
+import { reducer as formReducer } from "redux-form";
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
