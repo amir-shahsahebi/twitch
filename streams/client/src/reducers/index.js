0286cbbc73fc89bedfd89f7d9e4174b285b8b82b
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-05 04:25:13 +0430

    reducer & store updated

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -2,7 +2,9 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 // for installing redux form, we use:   npm install redux-form --legacy-peer-deps
 import { reducer as formReducer } from "redux-form";
+import streamReducer from "./streamReducer";
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  stream: streamReducer,
 });
