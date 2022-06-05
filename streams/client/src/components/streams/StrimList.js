1683d7e6eb13a2b57c8e55d38d8207d333528061
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-05 15:54:12 +0430

    edit & delete set for logged in user

--- streams/client/src/components/streams/StrimList.js
+++ streams/client/src/components/streams/StrimList.js
@@ -1,14 +1,28 @@
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
 import React from "react";
+import { Link } from "react-router-dom";
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -18,19 +32,37 @@ class StreamList extends React.Component {
       );
     });
   }
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     console.log(this.props.streams);
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.stream) };
+  return {
+    streams: Object.values(state.stream),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
