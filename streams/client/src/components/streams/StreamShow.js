6ed29ca7a3838934471348c2f76d02bcc49cc944
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-11 20:09:57 +0430

    crud operation completed

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,5 +1,34 @@
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+import { fetchStream, editStream } from "../../actions";
+
+const StreamShow = ({ fetchStream, streams, editStream }) => {
+  const params = useParams();
+  useEffect(() => {
+    // because of a user may use a link like "http://localhost:3000/streams/edit/5" so it return undefined because the App doesn't load steam list in this page, so we first using fetchStream in order to load data
+    fetchStream(params.id);
+  }, []);
+  const currentStream = streams[params.id];
+  // console.log(streams);
+  // console.log(currentStream);
+
+  if (!currentStream) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{currentStream.title}</h1>
+      <h5>{currentStream.description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state) => {
+  return {
+    streams: state.stream,
+  };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
