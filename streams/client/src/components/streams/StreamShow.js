2f5702dd9e8b45a865dcb919a24fbd7af715d801
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-11 22:31:47 +0430

    build player added to fix bugs

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -6,12 +6,25 @@ import flv from "flv.js";
 import { useRef } from "react";
 const StreamShow = ({ fetchStream, streams, editStream }) => {
   const params = useParams();
+  const videoRef = useRef();
   useEffect(() => {
     // because of a user may use a link like "http://localhost:3000/streams/edit/5" so it return undefined because the App doesn't load steam list in this page, so we first using fetchStream in order to load data
+
     fetchStream(params.id);
+    buildPlayer();
   }, []);
-  const videoRef = useRef();
   const currentStream = streams[params.id];
+  const buildPlayer = () => {
+    if (!currentStream) {
+      return;
+    }
+    const player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${params.id}.flv`,
+    });
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  };
   // console.log(streams);
   // console.log(currentStream);
 
